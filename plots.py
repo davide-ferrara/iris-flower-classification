a1dcd00d0cd9830e40ebcb44729d744264e19d35
davide-ferrara <davide98ferrara@gmail.com>
2025-06-28 00:13:52 +0200

    plot

--- plots.py
+++ plots.py
@@ -5,20 +5,26 @@ from NeuralNetwork import NeuralNetwork
 dataset_path = "dataset/test_set.data"
 nn = NeuralNetwork.load("model.pkl")
 
-train_loss_x = np.arange(0, nn.get_epochs())
-train_loss_y = nn.get_train_loss()
 
-test_loss_x = train_loss_x
-test_loss_y = nn.get_test_loss()
-# Creare il plot
-plt.plot(train_loss_x, train_loss_y, label="Train Loss", color="blue")
-plt.plot(test_loss_x, test_loss_y, label="Test Loss", color="orange")
+def error_loss_plot():
+    train_loss_x = np.arange(0, nn.get_epochs())
+    train_loss_y = nn.get_train_loss()
 
-plt.title("Errore totale su training e test")
-plt.xlabel("Epoch")
-plt.ylabel("Loss")
-plt.axhline(0, color="black", linewidth=0.5, ls="--")
-plt.axvline(0, color="black", linewidth=0.5, ls="--")
-plt.grid()
-plt.legend()
-plt.show()
+    test_loss_x = train_loss_x
+    test_loss_y = nn.get_test_loss()
+
+    # Creare il plot
+    plt.plot(train_loss_x, train_loss_y, label="Train Loss", color="blue")
+    plt.plot(test_loss_x, test_loss_y, label="Test Loss", color="orange")
+
+    plt.title("Errore totale su training e test")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.axhline(0, color="black", linewidth=0.5, ls="--")
+    plt.axvline(0, color="black", linewidth=0.5, ls="--")
+    plt.grid()
+    plt.legend()
+    plt.savefig("error_loss_plot.jpeg")
+
+
+error_loss_plot()
